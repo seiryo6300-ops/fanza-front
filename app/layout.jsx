199a59d56d4475dcd3bf42422097167fbf3d215a
seiryo6300-ops <seiryo6300@gmail.com>
2025-12-06 19:05:08 +0900

    Update layout.jsx

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,8 +1,4 @@
-export const metadata = {
-  title: "2000〜2010年代アーカイブ",
-  description: "熟女・人妻・懐かし黄金期コレクション",
-};
-
+"use client";
 import "./globals.css";
 
 export default function RootLayout({ children }) {
