69fe424c5dffce16ae617f02afe50580bcf8b3d5
seiryo6300-ops <seiryo6300@gmail.com>
2025-12-06 16:11:58 +0900

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
-/** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: false,
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "via.placeholder.com",
+      },
+    ],
+  },
 };
 
 module.exports = nextConfig;
