c7425a9a8cfc3a1007abdcbfd1a44321409c3067
seiryo6300-ops <seiryo6300@gmail.com>
2025-12-06 18:30:50 +0900

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,23 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: [
-      "pics.dmm.co.jp",
-      "www.dmm.co.jp",
-    ],
     remotePatterns: [
       {
         protocol: "https",
         hostname: "pics.dmm.co.jp",
         pathname: "/**",
-      },
-      {
-        protocol: "https",
-        hostname: "www.dmm.co.jp",
-        pathname: "/**",
-      },
+      }
     ],
-    unoptimized: true,  // ← これが最重要！
+    unoptimized: true,
   },
 };
 
